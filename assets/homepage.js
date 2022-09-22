7ab90b9bd399c75a4ae84d1e043aa580561eed64
Sheev <ericmarcypro@gmail.com>
2022-09-22 19:47:33 -0400

    added streaming availabilities for all main series harry potter movies, background images for homepage

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,8 +1,18 @@
-var myApiKey = "7J1uypi2AfYBuHtNW4z6895OlDo3EMWPn5ICsy6M";
+const myApiKey = "7J1uypi2AfYBuHtNW4z6895OlDo3EMWPn5ICsy6M";
+let linkArr = [
+"https://api.watchmode.com/v1/title/1159480/sources/?apiKey=", 
+"https://api.watchmode.com/v1/title/1159473/sources/?apiKey=", 
+"https://api.watchmode.com/v1/title/1159479/sources/?apiKey=", 
+"https://api.watchmode.com/v1/title/1159476/sources/?apiKey=", 
+"https://api.watchmode.com/v1/title/1159478/sources/?apiKey=",
+"https://api.watchmode.com/v1/title/1159477/sources/?apiKey=",
+"https://api.watchmode.com/v1/title/1159474/sources/?apiKey=",
+"https://api.watchmode.com/v1/title/1159475/sources/?apiKey="
+];
 
 
 function createApi(query) {
-    var searchQuery = "https://api.watchmode.com/v1/search/?apiKey="+myApiKey+"&search_field=name&search_value="+query
+    let searchQuery = linkArr[0]+myApiKey
 
     fetch(searchQuery)
     .then(function (response) {
