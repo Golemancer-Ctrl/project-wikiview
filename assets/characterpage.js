863007812e3f216ff1c22392257256eff7f21b40
Eric Marcy <ericmarcypro@gmail.com>
2022-10-02 13:43:50 -0400

    finished building out html base and importing info

--- assets/characterpage.js
+++ assets/characterpage.js
@@ -3,6 +3,13 @@ let selectedCharacterIndex = JSON.parse(localStorage.getItem("charIndex"));
 
 let sigText = document.querySelector('#sigSpell');
 let sigSpells = ["Stupefy", "Muffliato", "Wordless Stupefy", "Expelliarmus", "Sectumsempra"];
+let charQuotes = [
+    "I realize I never really thanked you properly for tipping me off about those dragons.",
+    "I’ve interrupted a deep thought, haven’t I? I can see it growing smaller in your eyes...",
+    "Honestly, if you were any slower, you’d be going backwards.",
+    "I don't go looking for trouble. Trouble usually finds me.",
+    "I can teach you how to bottle fame, brew glory, even stopper death… If you aren’t as big a bunch Of dunderheads as I usually have to teach."
+]
 
 console.log(selectedCharacterIndex);
 
@@ -23,22 +30,27 @@ function fetchApi() {
 fetchApi();
 
 let charInfo = JSON.parse(localStorage.getItem("charObject"));
+let quote = document.getElementById("quote");
 
 if (selectedCharacterIndex === 5) {
     sigText.innerText = sigSpells[0];
+    quote.innerText = charQuotes[0];
 } else if (selectedCharacterIndex === 3) {
     sigText.innerText = sigSpells[1];
+    quote.innerText = charQuotes[1];
 } else if (selectedCharacterIndex === 10) {
     sigText.innerText = sigSpells[2];
+    quote.innerText = charQuotes[2];
 } else if (selectedCharacterIndex === 0) {
     sigText.innerText = sigSpells[3];
+    quote.innerText = charQuotes[3];
 } else if (selectedCharacterIndex===7) {
     sigText.innerText = sigSpells[4];
+    quote.innerText = charQuotes[4];
 }
 
-for(i=0; i < document.getElementsByClassName("name").length; i++) {
-    document.getElementsByClassName("name")[i].innerText = charInfo.name;
-}
-
+document.querySelector("img").setAttribute("src", charInfo.image);
+document.querySelector("h1").innerText = charInfo.name;
 document.querySelector("#DOB").innerText = charInfo.dateOfBirth;
 document.querySelector("#house").innerText = charInfo.house;
+document.querySelector("#cite").innerText = "- " + charInfo.name;
